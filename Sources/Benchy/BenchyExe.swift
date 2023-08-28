6f3ca36a288e898a2ee8e18945eeaab936d5b8aa
Michael Redig <mredig@gmail.com>
2023-08-28 15:35:49 -0500

    (refactor) renamed BenchyExe

--- Sources/Benchy/BenchyExe.swift
+++ Sources/Benchy/BenchyExe.swift
@@ -6,11 +6,10 @@ struct BenchyExe {
 	static func main() async throws {
 //		try Benchy.addBenchyTest(NSDictionaryBufferToData.self)
 //		try Benchy.addBenchyTest(NSArrayBufferToData.self)
-
 		try Benchy.addBenchyTest(DateVsCFAbsoluteTimeGetCurrent.self)
 
 		try Benchy.runBenchmarks()
 
-		Benchy.displayResults()
+		Benchy.displayResults(decimalCount: 10)
 	}
 }
