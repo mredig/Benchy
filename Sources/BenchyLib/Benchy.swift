6947688c00ad18b05952b1b84da3a0fc8c9c850d
Michael Redig <mredig@gmail.com>
2023-08-28 15:36:37 -0500

    (refactor) renamed BenchyLib and refactor for instance management

--- Sources/BenchyLib/Benchy.swift
+++ Sources/BenchyLib/Benchy.swift
@@ -1,13 +1,14 @@
 import Foundation
 import Table
 
-public enum Benchy {
-	private static var benchyCollection: [any BenchyCollection.Type] = []
+public struct Benchy {
+	private var benchyCollection: [any BenchyComparator.Type] = []
 
-	internal static var results: [String: ResultStats] = [:]
+	internal var results: [String: ResultStats] = [:]
 
+	public private(set) static var shared = Benchy()
 
-	public static func addBenchyTest(_ benchmark: any BenchyCollection.Type) throws {
+	public mutating func addBenchyTest(_ benchmark: any BenchyComparator.Type) throws {
 		guard
 			benchyCollection.contains(where: { $0.id == benchmark.id }) == false
 		else { throw BenchyError.alreadyAddedCollection }
@@ -15,19 +16,30 @@ public enum Benchy {
 		benchyCollection.append(benchmark)
 		try benchmark.setupBenchmarks()
 	}
+	public static func addBenchyTest(_ benchmark: any BenchyComparator.Type) throws {
+		try shared.addBenchyTest(benchmark)
+	}
 
-	public static func runBenchmarks() throws {
+	public mutating func runBenchmarks(cleanup: Bool = true) throws {
 		for collection in benchyCollection {
 			let results = try collection.runCollections()
 			for result in results {
 				self.results[result.label] = result
 			}
+			if cleanup {
+				collection.cleanup()
+			}
 		}
 
-		benchyCollection.removeAll()
+		if cleanup {
+			benchyCollection.removeAll()
+		}
+	}
+	public static func runBenchmarks(cleanup: Bool = true) throws {
+		try shared.runBenchmarks(cleanup: cleanup)
 	}
 
-	public static func displayResults(clearingResultsAfterwards: Bool = false) {
+	public mutating func displayResults(decimalCount: Int = 4, cleanup: Bool = false) {
 		let sorted = results.sorted(by: {
 			$0.value.average < $1.value.average
 		})
@@ -37,8 +49,8 @@ public enum Benchy {
 		]
 
 		let numberFormatter = NumberFormatter()
-		numberFormatter.minimumFractionDigits = 4
-		numberFormatter.maximumFractionDigits = 4
+		numberFormatter.minimumFractionDigits = decimalCount
+		numberFormatter.maximumFractionDigits = decimalCount
 
 		for (label, stats) in sorted {
 			//		print("\(label) took an average of \(stats.average) taking at most \(stats.max) and \(stats.min) at its fastest. There was a \(stats.max - stats.min) delta between the extremities. The median time was \(stats.median)")
@@ -55,12 +67,15 @@ public enum Benchy {
 		do {
 			let table = try Table(data: tableData).table()
 			print(table)
-			if clearingResultsAfterwards {
+			if cleanup {
 				results.removeAll()
 			}
 		} catch {
 			print("Error creating table: \(error)")
 		}
+	}
+	public static func displayResults(decimalCount: Int = 4, cleanup: Bool = false) {
+		shared.displayResults(decimalCount: decimalCount, cleanup: cleanup)
 	}
 
 	enum BenchyError: Error {
